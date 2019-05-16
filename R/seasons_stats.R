97c1946bb3c2c478df05fc69329983d3a4c71f1b
hadadada00 <hadadada0000@gmail.com>
2019-05-17 04:01:46 +0900

    modify data definition

--- R/seasons_stats.R
+++ R/seasons_stats.R
@@ -4,14 +4,61 @@
 #'
 #' @format A data frame with 24,691 rows and 52 variables:
 #' \describe{
-#'   \item{Year}{year}
-#'   \item{Player}{name the player}
-#'   \item{Pos}{position of the player}
-#'   \item{position}{position of the player}
-#'   \item{Age}{Age of the player}
-#'   \item{Tm}{Team, in 3 letters}
-#'   ...
-#'   for details
+#'   \item{Year}{Year that the season occurred. Since the NBA season is split over two calendar years, the year given is the last year for that season. For example, the year for the 1999-00 season would be 2000.}
+#'   \item{Player}{Name}
+#'   \item{position}{Position}
+#'   \item{Age}{Age; player age on February 1 of the given season.}
+#'   \item{Tm}{Team}
+#'   \item{G}{Games}
+#'   \item{GS}{Games Started (available since the 1982 season)}
+#'   \item{MP}{Minutes Played (available since the 1951-52 season)}
+#'   \item{PER}{Player Efficiency Rating (available since the 1951-52 season); PER is a rating developed by ESPN.com columnist John Hollinger. In John's words, "The PER sums up all a player's positive accomplishments, subtracts the negative accomplishments, and returns a per-minute rating of a player's performance."}
+#'     \url{https://www.basketball-reference.com/about/per.html}
+#'   \item{TS%}{}
+#'   \item{3PAr}{}
+#'   \item{FTr}{}
+#'   \item{ORB%}{}
+#'   \item{DRB%}{}
+#'   \item{TRB%}{}
+#'   \item{AST%}{}
+#'   \item{TRB%}{}
+#'   \item{STL%}{}
+#'   \item{BLK%}{}
+#'   \item{TOV%}{}
+#'   \item{USG%}{}
+#'   \item{blanl}{}
+#'   \item{OWS}{}
+#'   \item{DWS}{}
+#'   \item{WS}{}
+#'   \item{WS/48}{}
+#'   \item{blank2}{}
+#'   \item{OBPM}{}
+#'   \item{DBPM}{}
+#'   \item{BPM}{}
+#'   \item{VORP}{}
+#'   \item{FG}{}
+#'   \item{FGA}{}
+#'   \item{FG%}{}
+#'   \item{3P}{}
+#'   \item{3PA}{}
+#'   \item{3P%}{}
+#'   \item{2P}{}
+#'   \item{2PA}{}
+#'   \item{2P%}{}
+#'   \item{eFG%}{}
+#'   \item{FT}{}
+#'   \item{FTA}{}
+#'   \item{FT%}{}
+#'   \item{ORB}{}
+#'   \item{DRB}{}
+#'   \item{TRB}{}
+#'   \item{AST}{}
+#'   \item{STL}{}
+#'   \item{BLK}{}
+#'   \item{TOV}{}
+#'   \item{PF}{}
+#'   \item{PTS}{}
+#'   \item{for details, see below.}
 #'   \url{https://www.basketball-reference.com/about/glossary.html}
 #' }
 #' @docType data
